f9b048060133c2a0bcaa72a747fea7e3383b7d32
Michael Law <michael.law@aggreko.com>
2024-04-28 17:27:08 +0100

    Local dev of NPM environment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,27 +2,27 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "../components/navbar";
-import { appWithTranslation } from 'next-i18next'
+import { appWithTranslation } from "next-i18next";
 import AppContextProvider from "../contexts/appContextProvider";
 import AppContext from "../contexts/appContext";
 import Head from "next/head";
-import { useTranslation } from 'next-i18next'
+import { useTranslation } from "next-i18next";
 import { useEffect } from "react";
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { SessionProvider } from "next-auth/react"
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { SessionProvider } from "next-auth/react";
 
-const OurGameApp = ({ Component, pageProps: {session, ...pageProps } }: AppProps) => {
-  const { t, i18n } = useTranslation('navbar', { bindI18n: 'languageChanged loaded' })
+const SquareGridApp = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
+  const { t, i18n } = useTranslation("navbar", { bindI18n: "languageChanged loaded" });
 
   useEffect(() => {
-    i18n.reloadResources(i18n.resolvedLanguage, ['navbar'])
-  }, [])
+    i18n.reloadResources(i18n.resolvedLanguage, ["navbar"]);
+  }, []);
 
   return (
     <SessionProvider session={session}>
       <AppContextProvider>
         <AppContext.Consumer>
-          {context =>
+          {(context) => (
             <>
               <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
@@ -30,17 +30,17 @@ const OurGameApp = ({ Component, pageProps: {session, ...pageProps } }: AppProps
               <Navbar t={t} />
               <Component {...pageProps} context={context} />
             </>
-          }
+          )}
         </AppContext.Consumer>
       </AppContextProvider>
     </SessionProvider>
   );
-}
+};
 
 export const getStaticProps = async ({ locale }: { locale: string }) => ({
   props: {
-    ...await serverSideTranslations(locale, 'navbar'),
+    ...(await serverSideTranslations(locale, "navbar")),
   },
-})
+});
 
-export default appWithTranslation(OurGameApp)
+export default appWithTranslation(SquareGridApp);
