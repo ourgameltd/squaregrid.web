9b6a3c331361fb24d6b98813806b5b678a82ebce
Michael Law <Michael.Law@aggreko.com>
2025-03-19 00:11:21 +0000

    Adding better content

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,55 +7,73 @@ const Home = () => {
   return (
     <>
       <Head>
-        <title>Be a winner on with Square Grid - Join Our Grid-Based Draws!</title>
-        <meta name="description" content="Experience the thrill of Square Grid! Participate in our unique grid-based games, enter draws, and win the card. Perfect for fundraisers and personal challenges. Try your luck today!" />
+        <title>Square Grid - Fundraising made easy!</title>
+        <meta name="description" content="Participate in our unique grid-based game, enter the game, claim some squares and allow your network to have a winner. Perfect for fundraisers and personal challenges, and if your a charity you could win 50% of all cards bought on this website as a donation to your charity. Try your luck today!" />
         <link rel="canonical" href="https://squaregrid.org" />
         <meta name="robots" content="index, follow" />
-        <meta property="og:title" content="Win Big with Square Grid - Join Our Grid-Based Game Draws!" />
-        <meta property="og:description" content="Participate in Square Grid for a chance to win exciting prizes! Dive into our grid-based draws and make your day a winning one. Great for fundraisers and thrilling competitions." />
+        <meta property="og:title" content="Square Grid - Fundraising made easy!" />
+        <meta property="og:description" content="Participate in our unique grid-based game, enter the game, claim some squares and allow your network to have a winner. Perfect for fundraisers and personal challenges, and if your a charity you could win 50% of all cards bought on this website as a donation to your charity. Try your luck today!" />
         <meta property="og:url" content="https://squaregrid.org" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://squaregrid.org/images/social.webp" />
-        <meta name="twitter:card" content="summary_large_image"/>
-        <meta name="twitter:title" content="Win Big with Square Grid - Join Our Grid-Based Game Draws!"/>
-        <meta name="twitter:description" content="Join the fun at Square Grid and win! Our grid-based games offer excitement and the thrill of winning. Don’t miss out—play now and be a winner!"/>
-        <meta name="twitter:url" content="https://squaregrid.org"/>
-        <meta name="twitter:image" content="https://squaregrid.org/images/social.webp"/>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Square Grid - Fundraising made easy!" />
+        <meta name="twitter:description" content="Participate in our unique grid-based game, enter the game, claim some squares and allow your network to have a winner. Perfect for fundraisers and personal challenges, and if your a charity you could win 50% of all cards bought on this website as a donation to your charity. Try your luck today!" />
+        <meta name="twitter:url" content="https://squaregrid.org" />
+        <meta name="twitter:image" content="https://squaregrid.org/images/social.webp" />
       </Head>
       <div className="untree_co-hero pb-0" id="home-section">
         <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <div className="row justify-content-center">
-                <div className="col-md-7 text-center mb-5">
-                  <h1 className="heading">
-                    Welcome to Square Grid <span className="fst-italic text-muted h5 d-block">Create and play grid-based draw games</span>
-                  </h1>
-                </div>
+          <div className="row justify-content-center">
+            <div className="jumbotron jumbotron-fluid">
+              <div className="container">
+                <h1 className="display-4 text-center">Charity Fundraising</h1>
+                <p className="lead">Made easy for you and you players, with a chance for your charity to win big!.</p>
               </div>
-              <div className="row align-items-center">
-                <div className="col-lg-4">
-                  <div className="intro">
-                    <div className="excerpt">
-                      <span className="caption">How it works?</span>
-                      <h2 className="font-weight-bold">Create a grid. Send a link. Pick a winner.</h2>
-                      <p>
-                        No more questions, no more sharing pictures on social media, no more hassle. Simply add your options, send the link and pick a winner.
-                      </p>
-                    </div>
-                    <p>
-                      <a href="/cards" className="btn btn-primary smoothscroll" >
-                        Try Now
-                      </a>
-                    </p>
-                  </div>
+            </div>
+          </div>
+          <div className="row align-items-center">
+            <div className="col-lg-6">
+              <div className="intro">
+                <div className="excerpt">
+                  <span className="caption">How does it work!</span>
+                  <h2 className="font-weight-bold">Create, Share & Win.</h2>
+                  <ol className="list-unstyled">
+                    <li>1. Create a card or let our AI do it!</li>
+                    <li>2. Share a link or QR and challenge your network!</li>
+                    <li>3. Players claim values to enter the game!</li>
+                    <li>4. A lucky player is chosen to win at random.</li>
+                    <li>5. Provide your charity number.</li>
+                    <li>5. Potentially win big each month.</li>
+                  </ol>
                 </div>
-                <div className="col-lg-8">
-                  <div className="illustration shadow">
-                    <img src="/images/sketch.svg" alt="Image" className="img-fluid" />
-                  </div>
-                </div>
+                <p>
+                  <a href="/cards" className="btn btn-primary smoothscroll" >
+                    Try Now
+                  </a>
+                </p>
               </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="illustration shadow">
+                <img src="/images/sketch.svg" alt="Image" className="img-fluid" />
+              </div>
+            </div>
+          </div>
+          <div className="row justify-content-center">
+            <div className="jumbotron jumbotron-fluid">
+              <div className="container">
+                <h2 className="display-4 text-center">Game for Good, <span className="badge badge-warning">50%</span> back!</h2>
+                <p className="lead">We pledge to donate half of everything we make back to a lucky charity each month.</p>
+              </div>
+            </div>
+            <div className="col-md-12 text-center mb-5">
+              <h1 className="heading">
+                <span className="fst-italic text-muted h5 d-block"></span>
+              </h1>
+              <p>
+                Not only are these games fun, they're also for a good cause. We believe in giving back, so 50% of all funds raised through the games in each country go to charity! Here's how it works: if you're a registered charity (with a valid charity number) and you create a card, you'll be entered into a random draw. One lucky charity creator is chosen at random to receive half of the total funds raised from all the games in your country. This means every time you make a game, upur giving your charity a chance win big. Play knowing that every game makes a difference. All charities are verified by number to keep things fair and legit.
+              </p>
             </div>
           </div>
         </div>
