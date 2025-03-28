217c3f2134cc1ebac6286d3d0599507bcf127dc1
Michael Law <michael@michaellaw.me>
2025-03-28 19:34:58 +0000

    Add existing privacy and terms pages to footer
    Fixes #27
    
    Add a footer with privacy and terms links to all pages.
    
    * **Add Footer Component**: Create a new `Footer` component in `components/Footer.tsx` with links to `/privacy` and `/terms` separated by a hyphen.
    * **Update _app.tsx**: Import the `Footer` component and add it to the bottom of the layout in `pages/_app.tsx`.
    * **Style Footer**: Add styles for the footer with a horizontal list of links in `public/scss/_site-footer.scss`.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/ourgameltd/squaregrid.web/issues/27?shareId=XXXX-XXXX-XXXX-XXXX).

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "../public/scss/style.scss";
 import type { AppProps } from "next/app";
 import Navbar from "../components/navbar";
+import Footer from "../components/Footer";
 import AppContextProvider from "../contexts/appContextProvider";
 import AppContext from "../contexts/appContext";
 import Head from "next/head";
@@ -21,6 +22,7 @@ const SquareGridApp = ({ Component, pageProps: { ...pageProps } }: AppProps) => 
             </Head>
             {!hideNavbar && <Navbar context={context} />}
             <Component {...pageProps} context={context} />
+            <Footer />
           </>
         )}
       </AppContext.Consumer>
